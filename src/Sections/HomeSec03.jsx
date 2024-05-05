8f54de75a8625dc938b6b8300b4fea35357f6afa
peter varga <pkvarg@yahoo.se>
2024-05-05 20:36:49 +0200

    conference sk en

--- src/Sections/HomeSec03.jsx
+++ src/Sections/HomeSec03.jsx
@@ -31,7 +31,7 @@ const HomeSec03 = ({ content, language }) => {
   return (
     <>
       <div className='bg-white h-8' id='events'></div>
-      <div className='bg-white pt-8 pb-16'>
+      <div className='bg-white pt-8 pb-16 px-2 lg:px-[20%]'>
         <h1 className='text-center text-[45px] uppercase mt-8'>
           {content.home03title}
         </h1>
@@ -47,7 +47,9 @@ const HomeSec03 = ({ content, language }) => {
                       <h3 className='text-[30px] text-center mt-2 mx-4 lg:mx-0'>
                         {blog.title}
                       </h3>
-                      <p className='mt-2'>{blog.text}</p>
+                      <p className='mt-2 lg:whitespace-pre text-center'>
+                        {blog.text}
+                      </p>
                       {/* <p>Sofia, Bulharsko</p> */}
                     </div>
                   )
@@ -57,7 +59,9 @@ const HomeSec03 = ({ content, language }) => {
                   <h3 className='text-[30px] text-center mt-2 mx-4 lg:mx-0'>
                     {blog.title}
                   </h3>
-                  <p className='mt-2'>{blog.text}</p>
+                  <p className='mt-2 lg:whitespace-pre text-center'>
+                    {blog.text}
+                  </p>
                   {/* <p>Sofia, Bulharsko</p> */}
                 </div>
               ))}
